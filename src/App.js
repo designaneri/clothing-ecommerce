5f372b468f6ece29051a3c0c3ed8cab05dfa8b94
Aneri Shah <aneri.shah@cygnetinfotech.com>
2023-04-11 16:45:13 +0530

    UI updates for Ecommerce homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './card.scss';
+import './styles/_global.scss';
+import Header from './components/Header';
 import HeartIcon from '@mui/icons-material/FavoriteBorder';
-import {IconButton, Grid,Button, Box, Typography,Container} from '@mui/material'
-import { createTheme,ThemeProvider } from '@mui/material/styles';
-import { Splide, SplideSlide,SplideTrack } from '@splidejs/react-splide';
+import {
+  IconButton,
+  Grid,
+  Button,
+  Box,
+  Typography,
+  Container,
+} from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
-import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import React, { useState, useEffect } from 'react';
 
 const theme = createTheme({
   status: {
@@ -23,143 +30,213 @@ const theme = createTheme({
   },
   typography: {
     fontFamily: ['Open Sans', 'sans-serif'].join(','),
-    subtitle1:{
+    subtitle1: {
       fontSize: 11,
       fontWeight: 500,
       fontFamily: ['Libre Baskerville', 'serif'].join(','),
     },
-    subtitle2:{
+    subtitle2: {
       fontSize: 20,
       textTransform: 'uppercase',
       fontWeight: 600,
       fontFamily: ['Libre Baskerville', 'serif'].join(','),
     },
-    body1:{
+    body1: {
       fontSize: 12,
     },
     h4: {
       fontFamily: ['Libre Baskerville', 'serif'].join(','),
-      letterSpacing: '-1.2px'
+      letterSpacing: '-1.2px',
     },
-    h6:{
+    h6: {
       fontFamily: ['Libre Baskerville', 'serif'].join(','),
-    }
-  }  
- })
+    },
+  },
+});
 
 const App = () => {
   const essentialItems = [
     {
       id: 1,
       title: 'Anna',
-      img:'img1.webp',
+      img: 'img1.webp',
       description: 'Oxford Woolen Coat - Brown',
-      price: '1,190.00'
+      price: '1,190.00',
     },
     {
       id: 2,
       title: 'BEXLEY',
-      img:'img2.webp',
+      img: 'img2.webp',
       description: 'Bexley Wool Rich Coat',
-      price: '2,190.00'
+      price: '2,190.00',
     },
     {
       id: 3,
       title: 'Mount Blanc',
-      img:'img3.webp',
+      img: 'img3.webp',
       description: 'Roger Herringbone Overcoat',
-      price: '2,990.00'
+      price: '2,990.00',
     },
     {
-      id:4,
-      title:'Arrow',
-      img:'img4.webp',
-      description:'Blake Wool Rich Blazer',
-      price:'1,879.00'
-    }
-  ]
+      id: 4,
+      title: 'Arrow',
+      img: 'img4.webp',
+      description: 'Blake Wool Rich Blazer',
+      price: '1,879.00',
+    },
+  ];
+
   return (
     <ThemeProvider theme={theme}>
-    <Splide hasTrack={ false } aria-label="My Favorite Images">
-    <SplideTrack>
-      <SplideSlide>
-        <img src="img/bannerImage2.jpg" alt="Image 1"/>
-        <div className='banner-box'>
-          <div className='bannerinner-box'>
-            <Typography variant='subtitle1' sx={{mb:2}}>New Collection</Typography>
-            <Typography variant='subtitle2' sx={{mb:3}}>Summer-Mellon Dress</Typography>
-            <Typography variant='body1'>Upgrade your wardrobe with a variation of styles and fits that are both feminine and relaxed.</Typography>
-            <div>
-                <Button color="black" disableElevation size="large" sx={{mt:3}} variant='contained'>Shop Now</Button>
-            </div>
-          </div>
-        </div>
-      </SplideSlide>
-      <SplideSlide>
-        <img src="img/bannerImage3.jpg" alt="Image 2"/>
-        <div className='banner-box'>
-          <div className='bannerinner-box'>
-            <Typography variant='subtitle1' sx={{mb:2}}>New Collection</Typography>
-            <Typography variant='subtitle2' sx={{mb:3}}>Summer-Mellon Dress</Typography>
-            <Typography variant='body1'>Upgrade your wardrobe with a variation of styles and fits that are both feminine and relaxed.</Typography>
-            <div>
-                <Button color="black" disableElevation size="large" sx={{mt:3}} variant='contained'>Shop Now</Button>
-            </div>
-          </div>
-        </div>
-      </SplideSlide>
-    </SplideTrack>
-    <div className="splide__progress">
-      <div className="splide__progress__bar" />
-    </div>
-    </Splide>
-    <Container maxWidth={false}>
-      <Typography variant='h4' sx={{mt: 3}} align='center'>Discover the Essentials</Typography>
-      <section>
-        <div className='card-container'>
-          <Grid container spacing={3}> 
-          {essentialItems.map(({id,title,img,description,price}) => (
-            <Grid item lg={3} key={id} flexGrow={1}>
-                <div className='feature-card card'>
-                  <div className='card-img-wrapper'>
-                      <img src={'img/' + img}/>
-                      <Button disableElevation color="black" fullWidth variant="contained" className='add-to-cart-btn'>Add to Cart</Button>  
-                  </div>
-                  <Box display='flex' alignItems='center' justifyContent='space-between'>
-                      <Typography variant='body1' sx={{fontWeight: 700, textTransform: 'uppercase'}}>{title}</Typography>
-                      <IconButton aria-label="delete" size="small">
-                        <HeartIcon fontSize="inherit" />
-                      </IconButton>
-                  </Box>
-                  <Typography variant='body1'>{description}</Typography>
-                  <Typography variant='body1' sx={{fontWeight: 700, textTransform: 'uppercase'}}>Rs. {price}</Typography>
+      <Header />
+      <div className='main-container'>
+      <Splide hasTrack={false} aria-label='My Favorite Images'>
+        <SplideTrack>
+          <SplideSlide>
+            <img src='img/bannerImage2.jpg' alt='Image 1' />
+            <div className='banner-box'>
+              <div className='bannerinner-box'>
+                <Typography variant='subtitle1' sx={{ mb: 2 }}>
+                  New Collection
+                </Typography>
+                <Typography variant='subtitle2' sx={{ mb: 3 }}>
+                  Summer-Mellon Dress
+                </Typography>
+                <Typography variant='body1'>
+                  Upgrade your wardrobe with a variation of styles and fits that
+                  are both feminine and relaxed.
+                </Typography>
+                <div>
+                  <Button
+                    color='black'
+                    disableElevation
+                    size='large'
+                    sx={{ mt: 3 }}
+                    variant='contained'
+                  >
+                    Shop Now
+                  </Button>
                 </div>
-            </Grid>
-          ))}
-          </Grid>
-        </div>
-      </section>
-      <section>
-      <Grid container spacing={3}> 
-      <Grid item sm={6}>
-        <div className='offset-images'>
-          <img src='img/img1.webp' className='image1'/>
-          <img src='img/img2.webp' className='image2'/>
-        </div>
-          
-      </Grid>
-      <Grid item sm={6} alignSelf="center">
-            <Typography variant='h4' sx={{mb:3}}>Follow us on Instagram</Typography>
-            <Typography variant='p' sx={{fontSize:18}}>Better outfit for every occasion and even better accessory <br /> to match it all.</Typography>
-            <div>
-              <Button color="black" disableElevation size="large" sx={{mt:3}} variant='contained'>@BrandName</Button>
+              </div>
             </div>
-      </Grid>
-      </Grid>
-      </section>
+          </SplideSlide>
+          <SplideSlide>
+            <img src='img/bannerImage3.jpg' alt='Image 2' />
+            <div className='banner-box'>
+              <div className='bannerinner-box'>
+                <Typography variant='subtitle1' sx={{ mb: 2 }}>
+                  New Collection
+                </Typography>
+                <Typography variant='subtitle2' sx={{ mb: 3 }}>
+                  Summer-Mellon Dress
+                </Typography>
+                <Typography variant='body1'>
+                  Upgrade your wardrobe with a variation of styles and fits that
+                  are both feminine and relaxed.
+                </Typography>
+                <div>
+                  <Button
+                    color='black'
+                    disableElevation
+                    size='large'
+                    sx={{ mt: 3 }}
+                    variant='contained'
+                  >
+                    Shop Now
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </SplideSlide>
+        </SplideTrack>
+        <div className='splide__progress'>
+          <div className='splide__progress__bar' />
+        </div>
+      </Splide>
+      <Container maxWidth={false}>
+        <Typography variant='h4' sx={{ mt: 3 }} align='center'>
+          Discover the Essentials
+        </Typography>
+        <section>
+          <div className='card-container'>
+            <Grid container spacing={3}>
+              {essentialItems.map(({ id, title, img, description, price }) => (
+                <Grid item lg={3} key={id} flexGrow={1}>
+                  <div className='feature-card card'>
+                    <div className='card-img-wrapper'>
+                      <img src={'img/' + img} />
+                      <Button
+                        disableElevation
+                        color='black'
+                        fullWidth
+                        variant='contained'
+                        className='add-to-cart-btn'
+                      >
+                        Add to Cart
+                      </Button>
+                    </div>
+                    <Box
+                      display='flex'
+                      alignItems='center'
+                      justifyContent='space-between'
+                    >
+                      <Typography
+                        variant='body1'
+                        sx={{ fontWeight: 700, textTransform: 'uppercase' }}
+                      >
+                        {title}
+                      </Typography>
+                      <IconButton aria-label='delete' size='small'>
+                        <HeartIcon fontSize='inherit' />
+                      </IconButton>
+                    </Box>
+                    <Typography variant='body1'>{description}</Typography>
+                    <Typography
+                      variant='body1'
+                      sx={{ fontWeight: 700, textTransform: 'uppercase' }}
+                    >
+                      Rs. {price}
+                    </Typography>
+                  </div>
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+        </section>
+        <section>
+          <Grid container spacing={3}>
+            <Grid item sm={6}>
+              <div className='offset-images'>
+                <img src='img/img1.webp' className='image1' />
+                <img src='img/img2.webp' className='image2' />
+              </div>
+            </Grid>
+            <Grid item sm={6} alignSelf='center'>
+              <Typography variant='h4' sx={{ mb: 3 }}>
+                Follow us on Instagram
+              </Typography>
+              <Typography variant='p' sx={{ fontSize: 18 }}>
+                Better outfit for every occasion and even better accessory{' '}
+                <br /> to match it all.
+              </Typography>
+              <div>
+                <Button
+                  color='black'
+                  disableElevation
+                  size='large'
+                  sx={{ mt: 3 }}
+                  variant='contained'
+                >
+                  @BrandName
+                </Button>
+              </div>
+            </Grid>
+          </Grid>
+        </section>
       </Container>
-      </ThemeProvider>
+      </div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,175 @@
+import React, { useState, useEffect } from 'react';
+import { Link as RouterLink, } from 'react-router-dom';
+import {
+  AppBar, 
+  Toolbar,
+  IconButton,
+  Drawer,
+  Link,
+  MenuItem,
+  Box,
+  styled,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+const HeaderWrap = styled(Box)(({ theme }) => ({
+  background: 'white',
+  position:'static',
+  color: '#222',
+  padding: '0 30px'
+}));
+const HeaderInner = styled(Box)(({ theme }) => ({
+  padding: '15px 0',
+  display: 'flex',
+  alignItems:'center'
+}));
+const Header = () =>{
+  const headersData=[
+    {
+      id: 1,
+      label: "Home",
+      href: "/Home"
+    },
+    {
+      id: 2,
+      label: "Home 2",
+      link: "/Home2"
+    },
+    {
+      id: 3,
+      label: "Home 3",
+      link: "/Home3"
+    }
+  ]
+  const [state, setState] = useState({
+    mobileView: false,
+    drawerOpen: false
+  });
+  const { mobileView, drawerOpen } = state;
+  const [scrolled, setScrolled] = useState(false);
+  var lastScrollTop = 0;
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+
+    setResponsiveness();
+    const handleScroll = () => {
+      // if (window.scrollY > 0) {
+      //   setScrolled(true);
+      // }
+      // else{
+      //   setScrolled(false);
+      // }
+      if (window.scrollY > 160) {
+        if(window.scrollY < lastScrollTop) {
+          setScrolled(false);
+        }
+        else if(window.scrollY > lastScrollTop) {
+          setScrolled(true);
+        }
+    }
+      lastScrollTop = window.scrollY;
+    };
+    window.addEventListener('resize', () => setResponsiveness());
+    window.addEventListener("scroll", () => handleScroll());
+    
+    return () => {
+      window.removeEventListener('resize', () => setResponsiveness());
+      window.removeEventListener("scroll", () => handleScroll());
+    };
+  }, []);
+  const displayMobile = () => {
+    const handleDrawerOpen = () =>
+    setState((prevState) => ({ ...prevState, drawerOpen: true }));
+    const handleDrawerClose = () =>
+      setState((prevState) => ({ ...prevState, drawerOpen: false }));
+    return (
+      <HeaderWrap className={`header ${scrolled ? "scrolled" : ""}`}>
+        <HeaderInner justifyContent={'start'}>
+        <IconButton
+          {...{
+            edge: 'start',
+            color: 'inherit',
+            'aria-label': 'menu',
+            'aria-haspopup': 'true',
+            onClick: handleDrawerOpen,
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer
+          {...{
+            anchor: "left",
+            open: drawerOpen,
+            onClose: handleDrawerClose,
+          }}
+        >
+          <div>{getDrawerChoices()}</div>
+        </Drawer>
+        <img src='img/logo-small.png' className='logo-small'/>
+      </HeaderInner>
+      </HeaderWrap>
+    );
+  };
+  const displayDesktop = () => {
+    return (
+      <HeaderWrap className={`header-outer ${scrolled ? "scrolled" : ""}`}>
+        <HeaderInner justifyContent={'space-between'} className={`header-top ${scrolled ? "scrolled" : ""}`}>
+            <div>
+              <a href={`tel:00-123-5678`}><LocalPhoneOutlinedIcon sx={{verticalAlign:'middle'}}/> 00-123-5678</a>
+            </div>
+            <div>
+                <img src='img/logo.png' className='logo-outer'/>
+            </div>
+            <div>
+                <AccountCircleOutlinedIcon sx={{verticalAlign:'middle'}}/> Aneri Shah 
+            </div>
+        </HeaderInner>
+        <HeaderInner justifyContent={'center'} className={`header-bottom ${scrolled ? "scrolled" : ""}`}>
+          {/* <IconButton
+            {...{
+              edge: 'start',
+              color: 'inherit',
+              'aria-label': 'menu',
+              'aria-haspopup': 'true',
+            }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+          {getDrawerChoices()}
+        </HeaderInner>
+      </HeaderWrap>
+    );
+  };
+  const getDrawerChoices = () => {
+    return headersData.map(({ label, href }) => {
+      return (
+        <Link
+          {...{
+            component: RouterLink,
+            to: href,
+            color: "inherit",
+            style: { textDecoration: "none" },
+            key: label,
+          }}
+        >
+          <MenuItem>{label}</MenuItem>
+        </Link>
+      );
+    });
+  };
+  
+  return (
+    <header>
+      <AppBar>
+          {mobileView ? displayMobile() : displayDesktop()}
+      </AppBar>
+    </header>
+  );
+}
+export default Header;
